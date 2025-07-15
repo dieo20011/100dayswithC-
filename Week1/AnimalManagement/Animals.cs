4370d671aa9ef8e62e64f37aac8a2d95e1db8875
Luan Duy Pham <luan.pham@tscmiami.com>
2025-07-15 14:33:14 +0700

    ok

---
+++ Week1/AnimalManagement/Animals.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalManagement
+{
+    interface Animals
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+        string Sound { get; set; }
+        EGender Gender { get; set; }
+        void MakeSound(string sound)
+        {
+            Console.WriteLine(sound);
+        }
+
+    }
+
+    public class Animal : Animals
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Sound { get; set; } = string.Empty;
+        public EGender Gender { get; set; }
+        public void MakeSound(string Sound)
+        {
+            Console.WriteLine($"The animal makes a sound: {Sound}");
+        }
+    }
+
+    public class Dog : Animal
+    { 
+
+    }
+
+    public class Duck : Animal
+    {
+
+    }
+
+    public class Cat : Animal
+    {
+    }
+
+}
---
+++ Week1/AnimalManagement/Program.cs
@@ -0,0 +1,21 @@
+﻿using AnimalManagement;
+
+namespace Animal_Mnagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+            animals.Add(new Dog { Name = "Lu", Age = 3, Gender = EGender.Male, Sound = "Woof 2" });
+            animals.Add(new Cat { Name = "Meo", Age = 2, Gender = EGender.Female, Sound = "Meow 2" });
+            animals.Add(new Duck { Name = "Vit", Age = 1, Gender = EGender.Male, Sound = "Quack 2" });
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"Name: {animal.Name}, Age: {animal.Age}, Gender: {animal.Gender}");
+                animal.MakeSound(animal.Sound);
+            }
+        }
+    }
+}
